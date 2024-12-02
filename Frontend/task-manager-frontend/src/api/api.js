817aefc0147e20884be556a6d94a882022354387
Vincent <154533673+Diezel3@users.noreply.github.com>
2024-12-02 18:11:02 +0000

    Enabled CORS to allow Frontend access

--- Frontend/task-manager-frontend/src/api/api.js
+++ Frontend/task-manager-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5223/api/task', // Update with backend URL
+    baseURL: 'http://localhost:5223/api', // Update with backend URL
 });
 
 export default api;
--- TaskManager/TaskManager.Api/Program.cs
+++ TaskManager/TaskManager.Api/Program.cs
@@ -3,6 +3,15 @@ using TaskManager.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+
 builder.Services.AddControllers();
 
 // Add services to the container
@@ -20,6 +29,8 @@ using (var scope = app.Services.CreateScope())
     dbContext.Database.Migrate();
 }
 
+// Enable CORS
+app.UseCors("AllowFrontend");
 
 app.UseDeveloperExceptionPage();
 app.MapControllers();
