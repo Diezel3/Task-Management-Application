e02061a45f5eb294a17770c68cd3bcb5bd5acb3b
Vincent <154533673+Diezel3@users.noreply.github.com>
2024-12-02 16:31:23 +0000

    Tested Error Handling

--- TaskManager/TaskManager.Api/obj/Debug/net8.0/TaskManager.Api.AssemblyInfo.cs
+++ TaskManager/TaskManager.Api/obj/Debug/net8.0/TaskManager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d4221cf176862fbf5edfb5e49da0061f66a4481")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70aeb01d754744e5ffdf3b0c0435172ba3d37a53")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
