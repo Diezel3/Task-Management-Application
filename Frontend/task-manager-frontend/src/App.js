e672230f82874ff5fa42ba2603d51801a8c53ffb
Vincent <154533673+Diezel3@users.noreply.github.com>
2024-12-09 12:13:41 +0000

    Testing Task Creation

--- Frontend/task-manager-frontend/src/App.js
+++ Frontend/task-manager-frontend/src/App.js
@@ -1,12 +1,23 @@
 import TaskList from "./components/TaskList";
+import TaskForm from './TaskForm';
 
-function App() {
+// function App() {
+//   return (
+//       <div>
+//           <h1>Task Manager</h1>
+//           <TaskList />
+//       </div>
+//   );
+// }
+
+
+const App = () => {
   return (
-      <div>
-          <h1>Task Manager</h1>
-          <TaskList />
-      </div>
+    <div>
+      <h1>Task Manager</h1>
+      <TaskForm />
+    </div>
   );
-}
+};
 
 export default App;
