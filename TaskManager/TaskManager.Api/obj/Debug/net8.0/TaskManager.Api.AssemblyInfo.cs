afdc01db344b963780445b3d07788c433ba4c174
Vincent <154533673+Diezel3@users.noreply.github.com>
2025-01-24 14:21:04 +0000

    Ef Migrations to add Identity Tables

--- TaskManager/TaskManager.Api/obj/Debug/net8.0/TaskManager.Api.AssemblyInfo.cs
+++ TaskManager/TaskManager.Api/obj/Debug/net8.0/TaskManager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0023072c171ee8f39a53ca3e9f90bcb201585f80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a169a3b4a349125af46cea3e0dafe0c203336f07")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
