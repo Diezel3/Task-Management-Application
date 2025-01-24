a169a3b4a349125af46cea3e0dafe0c203336f07
Vincent <154533673+Diezel3@users.noreply.github.com>
2025-01-24 14:18:53 +0000

    Typo correction

--- TaskManager/TaskManager.Api/Data/AppDbContext.cs
+++ TaskManager/TaskManager.Api/Data/AppDbContext.cs
@@ -1,11 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using TaskManager.Api.Model;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
+
 
 namespace TaskManager.Api.Data
 {
-    public class AppDbContext : IdentityDbContext<IdentityUser>
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Taskk> Tasks { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
--- TaskManager/TaskManager.Api/Model/ApplicationUser.cs
+++ TaskManager/TaskManager.Api/Model/ApplicationUser.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
-namespace TaskManager.Api.Models
+namespace TaskManager.Api.Model
 {
     public class ApplicationUser : IdentityUser
     {
