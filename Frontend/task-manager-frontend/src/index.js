a0afc8b442c4fb5353b26caf9ec0151de1d9a38c
Vincent <154533673+Diezel3@users.noreply.github.com>
2025-03-05 12:45:16 +0000

    Changed app default access back to App.js

--- Frontend/task-manager-frontend/src/index.js
+++ Frontend/task-manager-frontend/src/index.js
@@ -8,6 +8,6 @@ import Register from './Register';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Register />
+        <App />
     </React.StrictMode>
 );
