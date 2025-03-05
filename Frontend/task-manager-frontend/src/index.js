8c552591203e0b30216bed96f0ab985f17006d92
Vincent <154533673+Diezel3@users.noreply.github.com>
2025-03-05 15:28:49 +0000

    Modified Login & Register UI

--- Frontend/task-manager-frontend/src/index.js
+++ Frontend/task-manager-frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import './styles.css';
 import Register from './Register';
+import Login from './Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
